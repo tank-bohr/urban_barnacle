a4a5bca5745781488a77a3318c0b3f3ca023c401
Alexey Nikitin <tank@bohr.su>
2020-09-13 23:52:46 +0300

    generalized server

--- src/urban_barnacle.erl
+++ src/urban_barnacle.erl
@@ -1,3 +1,19 @@
 -module(urban_barnacle).
 
--export([]).
+-include("pokemons.hrl").
+
+-export([
+  start_pokemons/0
+]).
+
+start_pokemons() ->
+    Config = #{
+        url                   => "http://localhost:4000/api/pokemons",
+        name                  => pokemons,
+        model                 => ?POKEMON_MODEL,
+        record                => #pokemon{},
+        ets_opts              => [named_table, public, set, {keypos, #pokemon.name}],
+        http_timeout_seconds  => 5,
+        sync_interval_seconds => 60
+    },
+    urban_barnacle_server:start_link(Config).
--- src/urban_barnacle_server.erl
+++ src/urban_barnacle_server.erl
@@ -1,10 +1,8 @@
 -module (urban_barnacle_server).
 -include_lib("kernel/include/logger.hrl").
 
--include("pokemons.hrl").
-
 -export([
-    start_link/0
+    start_link/1
 ]).
 
 -behaviour(gen_server).
@@ -15,24 +13,42 @@
     handle_info/2
 ]).
 
--define(SERVER, ?MODULE).
--define(URL, "http://localhost:4000/api/pokemons").
--define(SYNC_INTERVAL_SECONDS, 60).
--define(HTTP_TIMEOUT_SECONDS, 5).
-
 -record(state, {
-    etag = "none" :: string(),
-    timer         :: timer:tref() | undefined
+    url                   :: string(),
+    name                  :: atom(),
+    model                 :: list(),
+    record                :: tuple(),
+    http_timeout_seconds  :: non_neg_integer(),
+    sync_interval_seconds :: non_neg_integer(),
+    etag = "none"         :: string(),
+    timer                 :: timer:tref() | undefined,
+    backoff               :: backoff:backoff()
 }).
 
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, {}, []).
+start_link(Config) ->
+    gen_server:start_link(?MODULE, Config, []).
 
-init({}) ->
-    ok = logger:set_primary_config(level, debug),
-    pokemons = ets:new(pokemons, [named_table, public, set, {keypos, #pokemon.name}]),
+init(#{
+    url                   := Url,
+    name                  := Name,
+    model                 := Model,
+    record                := Record,
+    ets_opts              := EtsOpts,
+    http_timeout_seconds  := HttpTimeoutSeconds,
+    sync_interval_seconds := SyncIntervalSeconds
+}) ->
     self() ! sync,
-    {ok, #state{}}.
+    ets:new(Name, EtsOpts),
+    State = #state{
+        url                   = Url,
+        name                  = Name,
+        model                 = Model,
+        record                = Record,
+        http_timeout_seconds  = HttpTimeoutSeconds,
+        sync_interval_seconds = SyncIntervalSeconds,
+        backoff               = backoff:init(2, 10)
+    },
+    {ok, State}.
 
 handle_call(_Request, _From, State) ->
     {reply, {error, unknown_call}, State}.
@@ -45,40 +61,63 @@ handle_info(sync, State) ->
 handle_info(_Info, State) ->
     {noreply, State}.
 
-sync(State) ->
-    {ok, Response} = httpc:request(get,
-        {?URL, [{"If-None-Match", State#state.etag}]},
-        [{timeout, timer:seconds(?HTTP_TIMEOUT_SECONDS)}],
-        [{body_format, binary}]),
+sync(#state{url = Url, http_timeout_seconds = HttpTimeoutSeconds} = State) ->
+    Request = {Url, [
+        {"If-None-Match", State#state.etag}
+    ]},
+    HttpOptions = [
+        {timeout, timer:seconds(HttpTimeoutSeconds)}
+    ],
+    Options = [
+        {body_format, binary}
+    ],
+    case httpc:request(get, Request, HttpOptions, Options) of
+        {ok, Response} ->
+            ?LOG_ERROR("Success request: ~s", [Url]),
+            on_success_response(Response, State);
+        {error, Reason} ->
+            ?LOG_ERROR("Error while http request ~s: ~p", [Url, Reason]),
+            on_error_response(Reason, State)
+    end.
+
+on_success_response(Response, #state{name = Name, sync_interval_seconds = SyncInterval} = State) ->
+    {_, Backoff} = backoff:succeed(State#state.backoff),
     ETag = case Response of
         {{_, 200, _}, RespHeaders, Body} ->
-            store(jsx:decode(Body, [return_maps, {labels, binary}])),
+            Data = jsx:decode(Body, [return_maps, {labels, binary}]),
+            store(Data, Name, State#state.model, State#state.record),
             extract_etag_from_headers(RespHeaders, State#state.etag);
         {{_, 304, _}, RespHeaders, _Body} ->
-            ?LOG_DEBUG("304 (Not Modified)"),
+            ?LOG_DEBUG("Resource `~p` didn't change", [Name]),
             extract_etag_from_headers(RespHeaders, State#state.etag);
         {{_, Status, _}, RespHeaders, Body} ->
-            ?LOG_ERROR("Couldn't fetch pokemons: ~p", [Status]),
+            ?LOG_ERROR("Couldn't fetch resource `~p`: ~p", [Name, Status]),
             ?LOG_DEBUG("Headers: ~p~nBody: ~p", [RespHeaders, Body]),
             State#state.etag
     end,
-    {ok, TRef} = timer:send_after(timer:seconds(?SYNC_INTERVAL_SECONDS), sync),
-    State#state{timer = TRef, etag = ETag}.
+    ?LOG_DEBUG("Next sync in ~p seconds", [SyncInterval]),
+    {ok, TRef} = timer:send_after(timer:seconds(SyncInterval), sync),
+    State#state{timer = TRef, backoff = Backoff, etag = ETag}.
 
-store(#{<<"data">> := Data}) ->
-    Model = emodel:compile(?POKEMON_MODEL, tuple),
-    Pokemons = lists:filtermap(fun(Item) -> from_map(Item, Model) end, Data),
-    ets:insert(pokemons, Pokemons).
+on_error_response(_Reason, State) ->
+    {Timeout, Backoff} = backoff:fail(State#state.backoff),
+    ?LOG_DEBUG("Retry in ~p seconds", [Timeout]),
+    {ok, TRef} = timer:send_after(timer:seconds(Timeout), sync),
+    State#state{timer = TRef, backoff = Backoff}.
 
-from_map(Item, Model) ->
-    case emodel:from_map(Item, #pokemon{}, Model) of
-        {ok, Pokemon} ->
-            {true, Pokemon};
-        {error, Errors} ->
-            [?LOG_WARNING("Invalid field ~s: ~p", [Field, Message]) ||
-                {Field, Message} <- Errors],
-            false
-    end.
+store(#{<<"data">> := Data}, Name, Model, Record) ->
+    Compiled = emodel:compile(Model, tuple),
+    Records = lists:filtermap(fun(Item) ->
+        case emodel:from_map(Item, Record, Compiled) of
+            {ok, ValidRecord} ->
+                {true, ValidRecord};
+            {error, Errors} ->
+                [?LOG_WARNING("Invalid field ~s: ~p", [Field, Message]) ||
+                    {Field, Message} <- Errors],
+                false
+        end
+    end, Data),
+    ets:insert(Name, Records).
 
 extract_etag_from_headers(Headers, Default) ->
     LHeaders = [{string:lowercase(N), V} || {N, V} <- Headers],
